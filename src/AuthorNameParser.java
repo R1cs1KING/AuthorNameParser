f926f7ee07908de85648c0d78272c2b0b47cc072
R1cs1KING <ricsi960413@gmail.com>
2022-08-05 21:15:07 +0200

    maximum one comma

--- src/AuthorNameParser.java
+++ src/AuthorNameParser.java
@@ -12,17 +12,25 @@ public class AuthorNameParser {
         if (isInputNameToParseConsistsValidCharacters(nameToParse)) {
 
         }
+
         return parsedName;
     }
 
     private boolean isInputNameToParseConsistsValidCharacters(String nameToParse) {
         // regex accepts latin characters
-        // and the following special characters: ,; .; '; - and space
-        if (nameToParse.matches("[\\p{IsLatin},.'\\-\\s]*")) {
+        // and the following special characters: .; '; - and space
+        // it can contain maximum one: ,
+        if (nameToParse.matches("[\\p{IsLatin}.'\\-\\s]*,?[\\p{IsLatin}.'\\-\\s]*")) {
             return true;
         }
         // TODO: error message to user
         System.out.println("The inserted name contains invalid characters");
+        return false;
+    }
+
+    private boolean hasNameToParseComma(String nameToParse) {
+
+
         return false;
     }
 }
